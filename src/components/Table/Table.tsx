ab99c2f01b841c57a2199a5b2650ac880304a599
RomanSamohwal <rsamohwal@gmail.com>
2020-11-28 01:47:46 +0300

    add redux toolkit

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const Table = (props: any) => {
+    return <table className = "table table-bordered" style={{cursor: "pointer"}}>
+        <thead>
+        <tr>
+            <th onClick={props.onSort.bind(null, 'id')}>ID</th>
+            <th onClick={props.onSort.bind(null, 'name')}>Name</th>
+            <th onClick={props.onSort.bind(null, 'jobsCount')}>jobsCount</th>
+            <th onClick={props.onSort.bind(null, 'startTime')}>startTime</th>
+            <th>Process</th>
+            <th>jobs list</th>
+        </tr>
+        </thead>
+        <tbody>
+        {props.data.map((item: any) => (
+               <tr>
+                   <td>{item.id}</td>
+                   <td>{item.name}</td>
+                   <td>{item.jobsCount}</td>
+                   <td>{item.startTime}</td>
+                   <td>any process..</td>
+                   <td key={item.id} onClick={() => props.onRowSelect(item.id)}>jobs list</td>
+                   <td><button onClick={()=>{props.delete(item.id)}}>delete</button> </td>
+               </tr>
+        ))}
+        </tbody>
+    </table>
+}
